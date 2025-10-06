bf8b68c66522e7b3549e80e8f8dfad8afedc7afa
Pratya Palee <pratya.palee@gmail.com>
2025-10-06 20:41:22 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
-  "projectId": "aiaproject",
-  "appId": "1:489542925105:web:5cfe6f1ee2c14c50ba8e10",
-  "apiKey": "AIzaSyDnxWSVyKFiJEFzMYgqujJoXKgxBd3nPGQ",
-  "authDomain": "aiaproject.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "489542925105"
+  apiKey: "AIzaSyDnxWSVyKFiJEFzMYgqujJoXKgxBd3nPGQ",
+  authDomain: "aiaproject.firebaseapp.com",
+  databaseURL: "https://aiaproject-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "aiaproject",
+  storageBucket: "aiaproject.appspot.com",
+  messagingSenderId: "489542925105",
+  appId: "1:489542925105:web:5cfe6f1ee2c14c50ba8e10"
 };
