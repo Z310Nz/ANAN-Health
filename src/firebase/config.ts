91c3a260c75e50d74af9050f0cf65a6a1e53cb4b
Pratya Palee <pratya.palee@gmail.com>
2025-10-06 20:31:27 +0000

    webchannel_blob_es2018.js:52  GET https://firestore.googleapis.com/googl

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-7321018391-f3764",
+  "projectId": "aiaproject",
   "appId": "1:489542925105:web:5cfe6f1ee2c14c50ba8e10",
   "apiKey": "AIzaSyDnxWSVyKFiJEFzMYgqujJoXKgxBd3nPGQ",
-  "authDomain": "studio-7321018391-f3764.firebaseapp.com",
+  "authDomain": "aiaproject.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "489542925105"
 };
